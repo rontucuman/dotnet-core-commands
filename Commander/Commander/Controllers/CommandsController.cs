4eb49709c3afecd820331bcddb2862c0c9b51a19
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-05-22 05:19:49 -0400

    Added Commands controller and basic GET endpoints

---
+++ Commander/Commander/Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class CommandsController : ControllerBase
+  {
+    private readonly MockCommanderRepo _repo = new MockCommanderRepo();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Command>> GetAllCommands()
+    {
+      var commandItems = _repo.GetAllCommands();
+
+      return Ok(commandItems);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Command> GetCommandById(int id)
+    {
+      var command = _repo.GetCommandById(id);
+
+      return Ok(command);
+    }
+  }
+}
---
+++ Commander/Commander/Data/ICommanderRepo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander.Models;
+
+namespace Commander.Data
+{
+  public interface ICommanderRepo
+  {
+    IEnumerable<Command> GetAllCommands();
+    Command GetCommandById(int id);
+  }
+}
