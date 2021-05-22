678f97c70cb374823ea5fcda3decbda46b9a33c0
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-05-22 05:50:21 -0400

    Added DBContext

---
+++ Commander/Commander/Data/CommanderContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commander.Data
+{
+  public class CommanderContext : DbContext
+  {
+    public CommanderContext(DbContextOptions<CommanderContext> options) : base(options)
+    {
+      
+    }
+
+    public DbSet<Command> Commands { get; set; }
+  }
+}
