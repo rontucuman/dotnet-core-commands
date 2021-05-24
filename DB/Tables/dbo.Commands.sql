ef5e122967bc8ddab601168b2abce239f7aad11a
Ronald Tucuman Alarcon <ronald.tucuman@outlook.com>
2021-05-23 20:34:30 -0400

    Added DB scripts

---
+++ DB/Tables/dbo.Commands.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Commands] (
+  [Id] [int] IDENTITY,
+  [HowTo] [nvarchar](250) NOT NULL,
+  [Line] [nvarchar](max) NOT NULL,
+  [Platform] [nvarchar](max) NOT NULL,
+  CONSTRAINT [PK_Commands] PRIMARY KEY CLUSTERED ([Id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
